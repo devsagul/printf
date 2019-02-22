2b50d9fbe340cced82ef1673889caa0cc6437bc7
Manfred dondarrion Balon swann <mbalon-s@e3r9p1.21-school.ru>
2019-02-22 17:35:05 +0300

    start work on libftprintf function

---
+++ libftprintf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/22 17:33:09 by mbalon-s          #+#    #+#             */
+/*   Updated: 2019/02/22 17:34:34 by mbalon-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <string.h>
+
+typedef struct	s_smartstr
+{
+	char		*str;
+	size_t		len;
+	size_t		size;
+}				t_smartstr
+
+int		ft_printf(const char *format, ...);
+
+#endif
