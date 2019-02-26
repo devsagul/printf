0fe195fe5004ce7ee73ef3cf48cbce1f38bf9432
Orpha Rhaegar frey <orhaegar@e4r7p11.21-school.ru>
2019-02-26 04:50:39 +0300

    really tired

--- ft_float_format.c
+++ ft_float_format.c
@@ -6,162 +6,106 @@
 /*   By: orhaegar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 19:47:18 by orhaegar          #+#    #+#             */
-/*   Updated: 2019/02/25 00:15:07 by orhaegar         ###   ########.fr       */
+/*   Updated: 2019/02/26 04:12:51 by orhaegar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define STRNUM_SIZE 4933
+#define LDH_EXP	0x7FFFLL
 
 #include "libftprintf.h"
-
-typedef struct		s_str_ld
-{
-	unsigned char	intg[STRNUM_SIZE];
-	unsigned char	flt[STRNUM_SIZE];
-	unsigned short	i_size;
-	unsigned short	f_size;
-}					t_str_ld;
-
-void	intg_inc(t_str_ld *nbr,  unsigned char base)
-{
-	short i;
-
-	i = 0;
-	--base;
-	while (i < nbr->i_size && nbr->intg[i] == base)
-	{
-		nbr->intg[i] = 0;
-		++i;
-	}
-	if (i == nbr->i_size && i < STRNUM_SIZE)
-	{
-		nbr->intg[i] = 1;
-		nbr->i_size += 1;
-	}
-	else
-		nbr->intg[i] += 1;
-	return ;
-}
-
-void	flt_inc(t_str_ld *nbr, unsigned char base)
-{
-	short i;
-
-	i = nbr->f_size;
-	--base;
-	while (i > -1 && nbr->flt[i] == base)
-	{
-		nbr->flt[i] = 0;
-		--i;
-	}
-	if (i == -1)
-		intg_inc(nbr, base + 1);
-	else
-		nbr->flt[i] += 1;
-	return ;
-}
-
-short		num_digits_bp(long double a)
-{
-	short i;
-
-	i = 1;
-	while ((a /= 10) >= 1)
-		++i;
-	return (i);
-}
-
-size_t		ft_float_format(char **pstr, long double a)
-{
-	short	i;
-	short	j;
-	char	tmp;
-	char	*str;
-
-	str = *pstr;
-	i = num_digits_bp(a);
-	while (a >= 1)
-		a /= 10;
-	j = 0;
-	while (j < i)
-	{
-		a *= 10;
-		tmp = (char)(a);
-		*str++ = tmp + '0';
-		a -= (long double)(tmp);
-		++j;
-	}
-	*str++ = '.';
-	while (a > 1)
-	{
-		a *= 10;
-		tmp = (char)(a);
-		*str++ = tmp + '0';
-		a -= (long double)(tmp);
-		++j;
-	}
-	*str = '\0';
-	return (size_t)(j);
-	/*
-	long double		i;
-	long double		f;
-	long double		tmp;
-	t_str_ld		res;
-	short			j;
-
-	i = a; // get va_list here
-
-	res.i_size = 0;
-	res.f_size = 0;
-	f = i;
-	tmp = 0;
-	while (i > 9)
-	{
-		tmp = (tmp + (i % 10)) * 10;
-		res.intg[res.i_size++] = (unsigned char)(i % 10);
-		i /= 10;
-	}
-	f -= tmp;
-	while (f > 0)
-	{
-		f *= 10;
-		res.flt[res.f_size++] = (unsigned char)(f % 10);
-		f -= f % 10;
-	}
-	*pstr = (char *)malloc(res.f_size + res.i_size + 2);
-	if (*pstr == NULL)
-		return (NULL);
-	j = res.i_size - 1;
-	while(j > -1)
-		*(*pstr)++ = res.intg[j--] + '0';
-	*(*pstr)++ = '.';
-	j = 0;
-	while (j < res.f_size - 1)
-		*(*pstr)++ = res.flt[j++] + '0';
-	**pstr = '\0';
-	return (res.f_size + res.i_size + 1);
-	*/
-}
-
 #include <limits.h>
 #include <float.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct	s_bignum
+{
+	short	base;
+	int		exp;
+	size_t	len;
+	char	*str;
+}				t_bignum;
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *tmp;
+	unsigned char ch;
+
+	ch = (unsigned char)c;
+	tmp = (unsigned char *)b;
+	while (len > 0)
+	{
+		*tmp = ch;
+		tmp++;
+		len--;
+	}
+	return (b);
+}
+
+char	*decompose_ld(long double a, t_bignum *b)
+{
+	unsigned long long	tmp;
+	size_t				i;
+
+	tmp = *((unsigned long long *)(&a) + 1);
+	b->exp = (int)((tmp & LDH_EXP) - 0x3fffLL);
+	if (b->exp > 64)
+		b->len = b->exp;
+	else if (b->exp < 0)
+		b->len = 64 - b->exp;
+	else
+		b->len = 64;
+	printf("1.%lu\n", b->len);
+	b->str = (char *)malloc((b->len + 1) * sizeof(char));
+	if (!b->str)
+		return (NULL);
+	if (b->exp < 0)
+		i = b->len;
+	else
+		i = 64;
+	b->str[b->len] = '\0';
+	ft_memset(b->str, '0', b->len);
+	tmp = *((unsigned long long *)(&a));
+	printf("2.%llx\n", tmp);
+	while (i--)
+	{
+		(b->str)[i] = (char)(tmp & 1) + 48;
+		tmp >>= 1;
+	}
+	return (b->str);
+}
+
+size_t		ft_float_format(char **pstr, long double a)
+{
+
+	return (0);
+}
+
 int		main(void)
 {
-	char	*str = (char *)malloc(5000);
-	double  b, c ,d;
-	long double a, f, g, t;
-	size_t shift;
+	long double l, tmp;
+	t_bignum	b;
 
-	f = LDBL_MAX - 89770240.0 + 0.1425235;
-	t = 1352352353451345134333.0;
-	a = 4.992342342342342349999999999999999912313213131;
-	
-	printf("%Lf\n", t);
-	shift = ft_float_format(&str, t);
-	printf("%s\n", str);
-	printf("1352352353451345134.14351345134513451\n");
+	tmp = 1;
+	l = 12415567346245353535356.0000001231230320;
+	printf("%.20Lf\n", l);
+//	t = (unsigned long long *)(&l);
+	decompose_ld(l, &b);
+//	pow = *(t + 1) & 0x7fffLL - 16383LL;
+//	*t = (*t & (1LL << 63)) ^ *t;
+//	l -= (long double)(1LL << 63);
+//	while (pow--)
+//		tmp *= 2;
+	printf("%s\n", b.str);
+//	while (l > 0)
+//	{
+//		l *= 10;
+//		printf("%c", (char)l + '0');
+//		l -= (long double)((char)l);
+//	}
+//	printf("%Lf\n", t);
+//	printf("%llx", );
+//	printf("%llx\n", a.dw.h);
 /*	printf("%.40Lf\n", a);
 	printf("\n%100.Lf\n", a);
 	printf("%40.Lf\n", a);
--- test.c
+++ test.c
@@ -5,13 +5,13 @@ int		pomporom(unsigned long long a);
 int		main(void)
 {
 	double a = 0x000000000001;
-	double b = 4.999999999999999;
+	double b = 100000000000000000000000000.0;
 	long long c;
 	int i = 0;
 	unsigned long long gg = *((unsigned long long*)(&b));
 
 	pomporom(gg);
-	printf("\n");
+	printf("\n%30.1lf\n", b);
 
 	return (0);
 }
