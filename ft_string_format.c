3d678f5c1c5c188f99e07d17e47c0e0ef8041478
Manfred dondarrion Balon swann <mbalon-s@e3r9p1.21-school.ru>
2019-02-28 21:19:45 +0300

    apply norme to ft_string_format

--- ft_string_format.c
+++ ft_string_format.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_string_format.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 19:32:43 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/02/25 20:04:06 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/02/28 21:16:01 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,26 @@ size_t				ft_string_format(char **pdst, t_specification spec,
 	int		len;
 	char	*str;
 	char	*arg;
+	char	fill;
 
 	if (spec.long_long_mod || spec.long_mod)
-	    return (ft_unicode_format(pdst, spec, ap));
+		return (ft_unicode_format(pdst, spec, ap));
 	arg = (char *)va_arg(ap, char *);
 	if (arg == NULL)
 		arg = "(null)";
 	len = ft_strlen(arg);
-	if (!spec.precision_set)
-		spec.precision = len;
-	if (len < spec.precision)
+	if (!spec.precision_set || len < spec.precision)
 		spec.precision = len;
 	if (spec.minwidth < spec.precision)
 		spec.minwidth = spec.precision;
 	str = (char *)malloc(sizeof(char) * (spec.minwidth + 1));
 	if (str == NULL)
 		return (0);
-	if (!spec.force_zeroes || spec.align_left)
-		ft_memset(str, ' ', sizeof(char) * spec.minwidth);
-	else
-		ft_memset(str, '0', sizeof(char) * spec.minwidth);
+	fill = !spec.force_zeroes || spec.align_left ? ' ' : '0';
+	ft_memset(str, fill, spec.minwidth);
 	str[spec.minwidth] = '\0';
-	if (!spec.align_left)
-		ft_strncpy(str + spec.minwidth - spec.precision, arg, spec.precision);
-	else
-		ft_strncpy(str, arg, spec.precision);
 	*pdst = str;
+	str = spec.align_left ? str : str + spec.minwidth - spec.precision;
+	ft_strncpy(str, arg, spec.precision);
 	return (spec.minwidth);
 }
