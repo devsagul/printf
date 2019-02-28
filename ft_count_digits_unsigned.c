14c5dae98395ae64250b8cb05b8268680069ac21
Manfred dondarrion Balon swann <mbalon-s@e3r9p1.21-school.ru>
2019-02-28 19:46:29 +0300

    apply norme to ft_count_digits_unsigned

--- ft_count_digits_unsigned.c
+++ ft_count_digits_unsigned.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_count_digits.c                                  :+:      :+:    :+:   */
+/*   ft_count_digits_unsigned.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 01:56:40 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/02/26 01:56:41 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/02/28 19:46:07 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int ft_count_digits_unsigned(unsigned long long int nbr, unsigned base)
+int		ft_count_digits_unsigned(unsigned long long int nbr, unsigned base)
 {
-    int     res;
+	int		res;
 
-    res = 0;
-    while (nbr != 0)
-    {
-        nbr /= base;
-        res++;
-    }
-    return (res);
+	res = 0;
+	while (nbr != 0)
+	{
+		nbr /= base;
+		res++;
+	}
+	return (res);
 }
